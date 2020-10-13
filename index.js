0a502ea331c0544741c40d38a84bb796474bf4bd
Ninetta11 <ninawelsh1@outlook.com>
2020-10-14 09:51:12 +1100

    Generated markdown function

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "What command should be run for testing?",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "APACHE 2.0",
+            "GPL 3.0",
+            "BSD 3",
+            "None"
+        ]
+    }
 ];
 
 // function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,36 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ##Description
+  ${data.description}
+
+  ##Table of Contents
+  1. [Installation](##Installation)
+  2. [Usage](##Usage)
+  3. [License](##License)
+  4. [Contributing](##Contributing)
+  5. [Tests](##Tests)
+  6. [Questions](##Questions)
+
+  ##Installation
+  To install the necessary dependencies, run the following command:
+  ${data.installation}
+
+  ##Usage
+  ${data.usage}
+
+  ##License 
+  This project is licensed under the ${data.license} license.
+
+  ##Contributing
+  ${data.contributing}
+
+  ##Tests
+  To run tests, run the following command:
+  ${data.tests}
+
+  ##Questions
+  If you have any questions about this repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.username}.
 `;
 }
 
