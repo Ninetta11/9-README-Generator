b4670fc09d36ec76458884dea814d24ad11ed3c9
Ninetta11 <ninawelsh1@outlook.com>
2020-10-18 09:51:34 +1100

    Fixed errors in links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,18 +3,18 @@ function generateMarkdown(data) {
   return `# ${data.title}
   ---
 
-  ![${data.license}](https://img.shields.io/badge/license/${data.license}-${data.title}-green)
+  ![${data.license}](https://img.shields.io/badge/license-${data.license}-green)
 
   ## Description
   ${data.description}
 
   ## Table of Contents
-  1. [Installation](## Installation)
-  2. [Usage](## Usage)
-  3. [License](## License)
-  4. [Contributing](## Contributing)
-  5. [Tests](## Tests)
-  6. [Questions](## Questions)
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
 
   ## Installation
   To install the necessary dependencies, run the following command:
@@ -27,14 +27,14 @@ function generateMarkdown(data) {
   This project is licensed under the ${data.license} license.
 
   ## Contributing
-  ${data.contributing}
+  ${data.contribution}
 
   ## Tests
   To run tests, run the following command:
   ${data.tests}
 
   ## Questions
-  If you have any questions about this repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [${data.username}](/${data.username}).
+  If you have any questions about this repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [${data.username}](https://www.github.com/${data.username}).
 `;
 }
 
